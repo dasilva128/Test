b73222cff62862f25edfa05b35b5e80405afa6c2
dasilva128 <hasoonhandavi@gmail.com>
2025-08-18 10:18:46 +0330

    Upload via bot

--- config.py
+++ config.py
@@ -9,12 +9,8 @@ API_HASH = os.getenv('API_HASH')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 DESTINATION_CHAT = os.getenv('DESTINATION_CHAT')
 
-PROXIES = [
-    'http://185.226.92.131:32737',
-    # Add more proxies here
-]
-
 CHANNELS_FILE = 'channels.txt'
 SITES_FILE = 'sites.txt'
 DB_FILE = 'search_results.db'
 LOG_FILE = 'bot.log'
+HTML_OUTPUT_DIR = 'html_output'  # Directory for HTML files
